57140486556a25ba8b71d549feb98ae07a96c6b1
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-12 17:21:06 -0700

    Initial Commit

---
+++ Assignment05/Assignment05App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment05App.swift
+//  Assignment05
+//
+//  Created by JPL-ST-SPRING2022 on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment05/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment05
+//
+//  Created by JPL-ST-SPRING2022 on 10/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
